0538fc61449f38cd55f0edad4f7dad56d01708a8
tertiux44 <secondsonofadebayo@gmail.com>
2022-12-06 10:48:14 +0100

    started

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 var navm = document.querySelector('.nav-mobile')
+var feat = document.querySelector('.features')
+var serv = document.querySelector('.services')
 
 function kaisen(){
     navm.classList.toggle('menu-drop')
+}
+
+function slash(){
+    feat.classList.toggle('slash')
+}
+
+function slashh(){
+    serv.classList.toggle('slash')
 }
