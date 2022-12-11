1dfa00878fc477506a29d7f72e9a3576e1fa6eed
tertiux44 <secondsonofadebayo@gmail.com>
2022-12-11 21:28:14 +0100

    completion

--- js/main.js
+++ js/main.js
@@ -13,3 +13,17 @@ function slash(){
 function slashh(){
     serv.classList.toggle('slash')
 }
+
+// document.querySelector('.chat')
+
+document.getElementById('chat').addEventListener('touchstart', ()=>{
+    // alert('hey')
+    document.querySelector('.chat').classList.add('whitechat')
+    
+})
+
+document.getElementById('chat').addEventListener('touchend', ()=>{
+    // alert('hey')
+    document.querySelector('.chat').classList.remove('whitechat')
+
+})
